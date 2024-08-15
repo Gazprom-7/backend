6e7a0d04899282176d80d9d1cd3ababf00ac8fed
Anton <patrin_toxa@mail.ru>
2024-08-15 19:44:57 +0500

    Add Education and Employee admins

--- backend/apps/employee/admin.py
+++ backend/apps/employee/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from models.address import Address
+
+from .models.address import Address
+from .models.education import Education
+from .models.employee import Employee
 
 
 @admin.register(Address)
@@ -7,3 +10,11 @@ class AddressAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
