9286ceb976a139f655d74ffd9ec08f235c780307
Anton <patrin_toxa@mail.ru>
2024-08-15 22:25:13 +0500

    Add Project and Team admins

--- backend/apps/team/admin.py
+++ backend/apps/team/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.projects import Project
+from .models.team import Team
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
