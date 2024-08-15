30e3a04aac63ca940a9b8916f9440c21e8186968
Anton <patrin_toxa@mail.ru>
2024-08-15 19:13:27 +0500

    Create Education model

---
+++ backend/apps/employee/models/education.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Education(models.Model):
+    type = models.CharField(_("Тип образования"), max_length=31)
+    university = models.CharField(_("Университет"), max_length=127)
+    beginning = models.DateField(_("Начало"), null=True, blank=True)
+    graduation = models.DateField(_("Выпуск"), null=True, blank=True)
