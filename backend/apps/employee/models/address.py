fe7ba185d872c24c903c0316c156ce7a2c9608b2
Anton <patrin_toxa@mail.ru>
2024-08-16 09:28:45 +0500

    Add property method

--- backend/apps/employee/models/address.py
+++ backend/apps/employee/models/address.py
@@ -6,5 +6,9 @@ class Address(models.Model):
     country = models.CharField(_("Страна"), max_length=31)
     timezone = models.CharField(_("Часовой пояс"), max_length=31)
 
+    @property
+    def full_address(self):
+        return f"{self.country}, {self.timezone}"
+
     def __str__(self):
-        return self.country
+        return self.full_address
