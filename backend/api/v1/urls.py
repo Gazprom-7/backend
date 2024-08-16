bbd32db56479b61c29f94c3d5abdbaeb19772875
Anton <patrin_toxa@mail.ru>
2024-08-16 11:33:40 +0500

    Add projects router

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .employee import views
+from .employee import views as employee_views
+from .project import views as project_views
 
 router = DefaultRouter()
-router.register("employee", views.EmployeeViewSet)
+router.register("employee", employee_views.EmployeeViewSet)
+router.register("projects", project_views.ProjectViewSet)
 urlpatterns = [
     path("", include(router.urls)),
 ]
