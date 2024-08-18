f99270426607e68960dd0dadfcc5207cbc2c0fea
Anton <patrin_toxa@mail.ru>
2024-08-18 21:48:08 +0500

    Rework Education model

--- backend/apps/employee/models/education.py
+++ backend/apps/employee/models/education.py
@@ -4,9 +4,12 @@ from django.utils.translation import gettext_lazy as _
 
 class Education(models.Model):
     type = models.CharField(_("Тип образования"), max_length=31)
-    university = models.CharField(_("Университет"), max_length=127)
-    beginning = models.DateField(_("Начало"), null=True, blank=True)
+    organization = models.CharField(_("Учебное заведение"), max_length=127)
+    beginning = models.DateField(_("Начало учебы"), null=True, blank=True)
     graduation = models.DateField(_("Выпуск"), null=True, blank=True)
+    additional = models.BooleanField(
+        _("Дополнительное образование"), null=True, blank=True
+    )
 
     def __str__(self):
-        return self.university
+        return self.organization
