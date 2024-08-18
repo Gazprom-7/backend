83100809933b5f1f256462e63d4aa5d3b9468a53
Anton <patrin_toxa@mail.ru>
2024-08-18 21:47:18 +0500

    Rework Employee model

--- backend/apps/employee/models/employee.py
+++ backend/apps/employee/models/employee.py
@@ -4,6 +4,8 @@ from django.utils.translation import gettext_lazy as _
 
 from .address import Address
 from .education import Education
+from .interest import Interest
+from .skills import Skill
 
 User = get_user_model()
 
@@ -15,6 +17,9 @@ class Employee(models.Model):
     password = models.CharField(_("Пароль"), max_length=127)
     telephone = models.CharField(_("Телефон"), max_length=15)
     telephone_work = models.CharField(_("Рабочий телефон"), max_length=15)
+    default_telephone = models.BooleanField(
+        _("Телефон по умолчанию"), default=True
+    )
     telegram = models.CharField(_("Телеграм"), max_length=127)
     email = models.EmailField(_("Электронная почта"))
     leadership = models.ForeignKey(
@@ -26,12 +31,15 @@ class Employee(models.Model):
         verbose_name=_("Руководитель"),
     )
     biography = models.TextField(_("Биография"), null=True, blank=True)
-    technology_stack = models.CharField(_("Стек навыков"), max_length=127)
     start_schedule = models.TimeField(_("Начало рабочего времени"))
     end_schedule = models.TimeField(_("Конец рабочего времени"))
     dey_week = models.CharField(_("Рабочие дни недели"), max_length=15)
-    start_absence = models.DateField(_("Начало отсутствия"))
-    end_absence = models.DateField(_("Конец отсутствия"))
+    start_absence = models.DateField(
+        _("Начало отсутствия"), null=True, blank=True
+    )
+    end_absence = models.DateField(
+        _("Конец отсутствия"), null=True, blank=True
+    )
     image = models.ImageField(
         _("Фото"),
         null=True,
@@ -40,20 +48,15 @@ class Employee(models.Model):
         default="default.jpg",
     )
     hobby = models.CharField(_("Хобби"), max_length=127, null=True, blank=True)
-    interest = models.CharField(
-        _("Интересы"), max_length=127, null=True, blank=True
-    )
     success = models.CharField(
         _("Успехи"), max_length=127, null=True, blank=True
     )
-    university = models.ForeignKey(
+    education = models.ManyToManyField(
         Education,
         verbose_name=_("Университет"),
-        on_delete=models.SET_NULL,
-        null=True,
+        blank=True,
     )
     languages = models.CharField(_("Языки"), max_length=127)
-    specialization = models.CharField(_("Специальность"), max_length=127)
     deputy = models.ForeignKey(
         "self",
         verbose_name=_("Заместитель"),
@@ -76,6 +79,13 @@ class Employee(models.Model):
         on_delete=models.SET_NULL,
         null=True,
     )
+    subordinates = models.ManyToManyField(
+        "self",
+        verbose_name=_("Подчиненные"),
+        blank=True,
+        related_name="superiors",
+        symmetrical=False,
+    )
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
@@ -84,6 +94,28 @@ class Employee(models.Model):
         blank=True,
         related_name="employee",
     )
+    teams = models.ManyToManyField(
+        "team.Team",
+        verbose_name=_("Команды"),
+        related_name="teams_employee",
+        blank=True,
+    )
+    projects = models.ManyToManyField(
+        "team.Project",
+        verbose_name=_("Проекты"),
+        blank=True,
+        related_name="projects_employee",
+    )
+    skills = models.ManyToManyField(Skill, verbose_name=_("Навыки"))
+    interest = models.ManyToManyField(Interest, verbose_name=_("Интересы"))
+
+    @property
+    def work_time(self):
+        return f"{self.start_schedule} - {self.end_schedule}"
+
+    @property
+    def absence(self):
+        return f"{self.start_absence} - {self.end_absence}"
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
