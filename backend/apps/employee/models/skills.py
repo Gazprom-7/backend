036b7bd4e0bd9419235acd882dc248240f99d30b
Anton <patrin_toxa@mail.ru>
2024-08-18 21:46:30 +0500

    Create skill table

---
+++ backend/apps/employee/models/skills.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Skill(models.Model):
+    name = models.CharField(_("Название навыка"), max_length=127)
+
+    def __str__(self):
+        return self.name
